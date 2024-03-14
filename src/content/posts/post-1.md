6687498d0a6ccc7537557ab8ea042e2c9464c46c
eb35 <roberthenry82@hotmail.com>
2024-03-14 00:41:41 -0500

    Finished lesson 28

--- src/content/posts/post-1.md
+++ src/content/posts/post-1.md
@@ -5,6 +5,8 @@ pubDate: 2024-03-08
 isDraft: false
 canonicalUrl: http://localhost:4321/posts/post-1
 author: christina
+cover: ./images/post-1.jpg
+coverAlt: "laptop in the dark"
 ---
 
 Content
--- src/schemas/postSchema.ts
+++ src/schemas/postSchema.ts
@@ -1,14 +1,20 @@
+import { Image } from 'astro:assets';
 import { reference, z } from "astro:content";
 
-const postSchema = z.object({
-    title: z.string().max(65, {
-        message: "Your title must be 65 characters or less",
-    }),
-    tags: z.array(z.string()),
-    pubDate: z.date(),
-    isDraft: z.boolean(),
-    canonicalUrl: z.string().url().optional(),
-    author: reference("team"),
-});
+const postSchema = ({ image }: any) => 
+    z.object({
+        title: z.string().max(65, {
+            message: "Your title must be 65 characters or less",
+        }),
+        tags: z.array(z.string()),
+        pubDate: z.date(),
+        isDraft: z.boolean(),
+        canonicalUrl: z.string().url().optional(),
+        author: reference("team"),
+        cover: image().refine((img: any) => img.width >= 1000, {
+            message: "Cover image must be at least 1000 pixels wide!",
+        }),
+        coverAlt: z.string(),
+    });
 
 export default postSchema;
