0a50ee34b044f3471321c968611b843df9ffc6e7
eb35 <roberthenry82@hotmail.com>
2024-04-12 00:16:54 -0500

    Lessons 38 - 42
    Starting new module/project. Added layout, nav, and footer.

---
+++ src/data/siteData.ts
@@ -0,0 +1,32 @@
+export const baseData = {
+    title: "Rob Henry Portfolio",
+    description: "Welcome to my portfolio site! I'm a self-taught designer and developer obsessed with quality.",
+};
+
+export const socialData = [
+    {
+        title: "dribbble",
+        href: "https://dribbble.com/",
+        icon: "icon--dribbble",
+    },
+    {
+        title: "instagram",
+        href: "https://instagram.com/",
+        icon: "icon--instagram",
+    },
+    {
+        title: "twitter",
+        href: "https://twitter.com/",
+        icon: "icon--twitter",
+    },
+    {
+        title: "youtube",
+        href: "https://youtube.com/",
+        icon: "icon--youtube",
+    },
+    {
+        title: "github",
+        href: "https://github.com/",
+        icon: "icon--github",
+    },
+];
