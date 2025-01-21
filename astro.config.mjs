6731e97d31c008893726267f31e433c8d351488e
eb35 <roberthenry82@hotmail.com>
2025-01-21 00:45:41 -0600

    64 Sitemap and prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 import icon from "astro-icon";
-
 import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  prefetch: {
+    prefetchAll: true
+  },
+  site: "https://robsportfolio.netlify.app",
+  integrations: [icon(), sitemap()],
   output: "static",
   adapter: netlify()
 });
