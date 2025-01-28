e742ea91663b7414185a44e2bc1379488072f45e
eb35 <roberthenry82@hotmail.com>
2025-01-27 23:45:36 -0600

    80 SSR Endpoint

--- src/pages/api/[id].json.ts
+++ src/pages/api/[id].json.ts
@@ -1,15 +1,13 @@
+export const prerender = false;
+
 import type { APIRoute } from "astro";
 import { getCollection, getEntry, type CollectionEntry } from "astro:content";
 
-export const GET: APIRoute = async ({ params, request }) => {
+export const GET: APIRoute = async ({ params, request, redirect }) => {
     const id = params.id as CollectionEntry<"blog">["id"];
     const post = await getEntry("blog", id);
+    if (!post) {
+        return redirect("/404");
+    }
     return new Response(JSON.stringify(post));
-}
-
-export async function getStaticPaths() {
-    const posts = await getCollection('blog');
-    return posts.map((post) => ({
-        params: { id: post.id },
-    }))
 }
