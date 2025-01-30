745ee41760247de768b5f5f50567fef67e2dfab3
eb35 <roberthenry82@hotmail.com>
2025-01-30 02:23:51 -0600

    86 Patch links

--- src/pages/api/[id].json.ts
+++ src/pages/api/[id].json.ts
@@ -25,3 +25,34 @@ export const DELETE: APIRoute = async ({ params }) => {
         return new Response(JSON.stringify({ message: "There was an unknown error" }), { status: 500 });
     }
 }
+
+export const PATCH: APIRoute = async ({ request, params }) => {
+    const id = params.id;
+    const { isRead } = await request.json();
+
+    try {
+        if (!id) {
+            throw new Error("No id provided");
+        }
+
+        const req = await fetch(`http://localhost:3000/links/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ isRead}),
+        });
+
+        if (!req.ok) {
+            throw new Error("There was a problem updating the json server");
+        }
+        
+        return new Response(null, { status: 204 });
+    } catch (e) {
+        console.error(e);
+        if (e instanceof Error) {
+            return new Response(JSON.stringify({ message: e.message }), { status: 500 });
+        }
+        return new Response(JSON.stringify({ message: "There was an unknown error" }), { status: 500 });
+    }
+}
