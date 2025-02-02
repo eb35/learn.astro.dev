5ee92545d012ac6dc94f2ae156727c32765e7310
eb35 <roberthenry82@hotmail.com>
2025-02-02 00:09:17 -0600

    89-90 Seeding db and displaying

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,25 @@
-import { db } from 'astro:db';
+import { db, Links } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(Links).values([
+		{
+			title: "Title",
+			url: "google.com",
+			isRead: true,
+			upvoteNum: 3,
+		},
+		{
+			title: "Coding in Public",
+			url: "codinginpublic.dev",
+			isRead: false,
+			upvoteNum: 10000,
+		},
+		{
+			title: "Blog",
+			url: "therobhenry.com",
+			isRead: true,
+			upvoteNum: 1234,
+		}
+	]);
 }
