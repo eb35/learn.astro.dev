8cafb8f22450d8e3d777239d65fe8f89a0d90997
eb35 <roberthenry82@hotmail.com>
2025-02-07 23:59:52 -0600

    Setting up project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,18 @@ import { defineConfig } from 'astro/config';
 
 import db from '@astrojs/db';
 
+import icon from 'astro-icon';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  integrations: [db(), icon()],
+  output: "server",
+  adapter: netlify(),
+  vite: {
+    plugins: [tailwindcss()]
+  },
 });
