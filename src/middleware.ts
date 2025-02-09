6cfa894e3dcb215146df312885b8f5df3faaf2c1
eb35 <roberthenry82@hotmail.com>
2025-02-09 00:33:05 -0600

    98-100 Testing middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,33 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-    console.log('in middleware');
+    // const { url } = context;
+    // if (url.pathname === "/") {
+    //     return Response.redirect(new URL("/redirected", url), 302);
+    // }
+    const pathname = context.url.pathname;
+    context.locals.userMessage = () => {
+        // pathname check
+        // auth status
+        const authStatus = true;
+        if (pathname === "/" && !authStatus) {
+            return "You must be logged in.";
+        }
+        if (pathname === "/") {
+            return "Freedom!"
+        }
+        return "";
+    };
+
+    if (pathname === "/") {
+        const response = await next();
+        const html = await response.text();
+        const redactedHtml = html.replaceAll("PRIVATE INFO", "REDACTED");
+        return new Response(redactedHtml, {
+            status: 200,
+            headers: response.headers
+        });
+    }
+
     return next();
 })
