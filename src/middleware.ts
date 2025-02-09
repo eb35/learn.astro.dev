12c4f766b6dbfda3a8be5ac14cf4bed8f754a50c
eb35 <roberthenry82@hotmail.com>
2025-02-09 00:50:13 -0600

    101 Logging errors
    Using Astro DB

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,15 @@
+import { db, Logs } from "astro:db";
 import { defineMiddleware } from "astro:middleware";
+import { date } from "astro:schema";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-    // const { url } = context;
-    // if (url.pathname === "/") {
-    //     return Response.redirect(new URL("/redirected", url), 302);
-    // }
-    const pathname = context.url.pathname;
-    context.locals.userMessage = () => {
-        // pathname check
-        // auth status
-        const authStatus = true;
-        if (pathname === "/" && !authStatus) {
-            return "You must be logged in.";
-        }
-        if (pathname === "/") {
-            return "Freedom!"
-        }
-        return "";
-    };
-
-    if (pathname === "/") {
-        const response = await next();
-        const html = await response.text();
-        const redactedHtml = html.replaceAll("PRIVATE INFO", "REDACTED");
-        return new Response(redactedHtml, {
-            status: 200,
-            headers: response.headers
-        });
+    const request = await next();
+    if (request.status !== 200) {
+        console.log("logging error");
+        await db.insert(Logs).values({
+            url: context.url.pathname,
+            date_accessed: new Date(),
+        })
     }
-
     return next();
 })
