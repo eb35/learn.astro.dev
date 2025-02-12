402eda2ecde449e32508c29ce4612f6c97e27654
eb35 <roberthenry82@hotmail.com>
2025-02-12 00:40:27 -0600

    108-109 Removing firebase, Adding Lucia

---
+++ db/config.ts
@@ -0,0 +1,23 @@
+import { column, defineDb, defineTable, NOW } from 'astro:db';
+
+const Session = defineTable({
+  columns: {
+    id: column.text({unique: true}),
+    expiresAt: column.number(),
+    userId: column.text({references: () => User.columns.id})
+  }
+});
+
+const User = defineTable({
+  columns: {
+    id: column.text({unique: true, primaryKey: true}),
+    username: column.text({unique: true}),
+    password_hash: column.text(),
+    createdAt: column.date({default: NOW})
+  }
+});
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: { User, Session }
+});
