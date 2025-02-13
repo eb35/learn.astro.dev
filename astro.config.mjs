9e973c8cef17ff6d1e117c972a5020b8a54878f4
eb35 <roberthenry82@hotmail.com>
2025-02-13 00:54:21 -0600

    115-118 Transitions setup and first steps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon(), sitemap(), react()],
+  output: "static",
+  adapter: netlify()
+});
