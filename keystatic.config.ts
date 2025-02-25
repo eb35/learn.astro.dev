1c0d6e05c14d25a14070e9a42649801f4864f3b1
eb35 <roberthenry82@hotmail.com>
2025-02-25 01:26:42 -0600

    5.139-140 Testimonial config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -47,8 +47,70 @@ export default config({
                             isRequired: true
                         }
                     })
+                }),
+                testimonials: fields.array(fields.relationship({
+                    label: "Testimonials",
+                    collection: "testimonials"
+                }),
+                {
+                    label: "Testimonials",
+                    validation: { length: { min: 1 } },
+                    itemLabel: (props) => props.value ?? "Testimonial",
                 })
             }
         })
+    },
+    collections: {
+        testimonials: collection({
+            label: "Testimonials",
+            path: "src/content/testimonial/*",
+            format: { data: "yaml" },
+            slugField: "name",
+            schema: {
+                name: fields.slug({ name: { label: "Name" } }),
+                role: fields.text({
+                    label: "Role",
+                    validation: {
+                        isRequired: true,
+                    }
+                }),
+                content: fields.text({
+                    label: "Testimonial Content",
+                    validation: {
+                        isRequired: true,
+                    },
+                    multiline: true
+                }),
+                date: fields.date({
+                    label: "Testimonial Date",
+                    defaultValue: { kind: "today" },
+                    validation: {
+                        isRequired: true,
+                    }
+                }),
+                isFeatured: fields.checkbox({
+                    label: "Feature this testimonial?",
+                    defaultValue: false
+                }),
+                rating: fields.number({
+                    label: "Rating",
+                    defaultValue: 5,
+                    step: 1,
+                    validation: {
+                        isRequired: true,
+                        min: 1,
+                        max: 5
+                    }
+                }),
+                image: fields.image({
+                    label: "Headshot",
+                    description: "Headshot for testimonial person",
+                    validation: {
+                        isRequired: true
+                    },
+                    publicPath: "src/assets/testimonial/"
+                })
+            }
+        }),
     }
 });
