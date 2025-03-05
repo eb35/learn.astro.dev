f18f967ae04607e784b1054046f0ef1941b35671
eb35 <roberthenry82@hotmail.com>
2025-03-04 23:20:32 -0600

    17.160 Project setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    image: {
+        domains: ["files.stripe.com"],
+    },
+    output: "server",
+    adapter: vercel(),
+    integrations: [icon()]
+});
